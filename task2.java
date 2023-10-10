5ae1ed98979ca759978ede57d3a77f8a0997db89
NetworkCrab <network123crab@gmail.com>
2023-10-10 22:05:42 +0530

    this is the second task

---
+++ task2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class task2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+            int a = sc.nextInt();
+        System.out.print("Enter the second number:");
+            int b = sc.nextInt();
+            int add = a + b;
+        System.out.println("The result is: "+add);
+    }
+}
