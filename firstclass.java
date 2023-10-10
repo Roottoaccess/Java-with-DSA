c6d6909a88bcff507c69a1439b01785a6651f1a2
NetworkCrab <network123crab@gmail.com>
2023-10-10 21:17:20 +0530

    first task

--- firstclass.java
+++ firstclass.java
@@ -1,6 +1,7 @@
 class Firstclass {
     public static void main(String[]args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World with Java !");
+        System.out.print("This is the next line");
     }
 }
