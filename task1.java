c9c23b862e2f3960016643656df9e9a3ac06523e
NetworkCrab <network123crab@gmail.com>
2023-10-10 21:48:45 +0530

    -avariable in java

--- task1.java
+++ task1.java
@@ -1,3 +1,15 @@
 public class task1 {
-    
+    public static void main(String[]args){
+        // Printing the pattern
+        /*
+
+        *
+        **
+        ***
+        ****
+        
+        without using the nested loop function
+        */
+        System.out.print("*\n**\n***\n****");
+    }
 }
