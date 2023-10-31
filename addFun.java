7558423b35d5ce0ba8342ce94b434ef121ceb6f2
NetworkCrab <network123crab@gmail.com>
2023-10-31 21:44:14 +0530

    add function

--- addFun.java
+++ addFun.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class addFun {
 
-    public static void addNum(int num1,int num2){
-        System.out.print("The result is -> "+(num1 + num2));
-        return;
+    public static int addNum(int num1,int num2){
+        int result = num1 + num2;
+        System.out.print("The result is -> "+result);
+        return result;
     }
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
